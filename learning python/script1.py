9e6511143bfa5d53f1924a2602101db712363725
Elisha Garcia <garciaelisha93@yahoo.com>
2024-06-04 15:58:19 -0500

    Add files via upload

---
+++ learning python/script1.py
@@ -0,0 +1,7 @@
+#a first python script
+import sys                      #load a library module
+print(sys.platform)
+print(2 ** 80)              #raise 2 to a power
+x = 'Spam! '
+print(x * 8)                    #string repetition
+input('press ENTER to exit')                           # <== ADDED for icon click on windows
---
+++ learning python/test.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Number 2")
+
+x = "Your mom"
+print(x)
+x = "sorry, that was rude"
+print(x)
+z = 1
+print(z)
+z = "one"
+print(z)
+#this should be a comment and should not effect the code
+print('this is a full string using single quotes')
+print('letter' [5])
