c4abe7b9f31f206b521d4032e6097314a809d1d8
Elisha Garcia <garciaelisha93@yahoo.com>
2024-09-05 22:27:13 -0500

    Add files via upload

--- learning python/fileioPractice.py
+++ learning python/fileioPractice.py
@@ -1,3 +1,4 @@
+
 file = open("my_data.txt", "w")
 file.write('Hello world!\n')
 file.write('This should be a new line.')
@@ -13,3 +14,23 @@ file.close()
 with open('my_data.txt', 'r') as file:
     content = file.read()
     print(content)
+
+
+with open('my_data.txt', 'r') as file:
+    for line in file:
+        print(line.strip())
+
+try:
+    with open('non_existent_file.txt', 'r') as file:
+        content = file.read()
+except FileNotFoundError:
+    print('The file does not exist')
+
+
+import os
+
+if os.path.exists('my_data.txt'):
+    os.remove('my_data.txt')
+else:
+    print('The file cannot be deleted because it does not exist.')
+
